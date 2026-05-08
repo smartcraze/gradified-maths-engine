5537dfe1750c1d7a59f4a90018c7ed2217cc902f
smartcraze <dev.surajv@gmail.com>
2026-05-09 02:48:06 +0530

    build final

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
+		host: "0.0.0.0",
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
+				target: "http://backend:3000",
 				changeOrigin: true,
 			},
 		},
